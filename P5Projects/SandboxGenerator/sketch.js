2e8d6eb4ca3dbaa2685af3642fa0dbce8fc4224e
ZLecter <gzlecter@gmail.com>
2018-05-29 22:32:51 -0500

    Flowers generation

--- P5Projects/SandboxGenerator/sketch.js
+++ P5Projects/SandboxGenerator/sketch.js
@@ -6,11 +6,17 @@ let nLevel = 5;
 let iSeed;
 let sLevels;
 let bt;
+let cbFlower;
+let sFlower;
+
+let noiseValues;
 
 function setup(){
 	createCanvas(200,200);
 	noLoop();
-	// framerate(30);
+
+	noiseValues = new Array(width*height);
+
 	createP();
 	bt = createButton('Redraw');
 	bt.mousePressed(CalcDraw);
@@ -18,12 +24,16 @@ function setup(){
 	sLevels = new Array(nLevel);
 
 	iSeed = createInput('Seed');
+	createP('');
+	cbFlower = createCheckbox('Flowers?', false);
+	sFlower = createSlider(0,nLevel-2, 0, 1);
 	createP('Noise Scale');
 	noiseScale = createSlider(0.01, 0.05, 0.05, 0.01);
 
 	for(let i = 0; i < nLevel; i++){
-		createP('Level ' + (nLevel - 1 - i));
+		createP('Level ' + i);
 		sLevels[i] = createSlider(0, 255, (255/nLevel) * i + 50);
+		console.log(i + " " + sLevels[i].value());
 	}
 
 }
@@ -45,12 +55,46 @@ function draw(){
 			for (let k = 0; k < nLevel; k++) {
 				if(n < sLevels[k].value()){
 					stroke((255/nLevel) * k);
+					noiseValues[i + j*width] = (255/nLevel) * k;
 					break;
 				}
 			}
 
 			point(i, j);
 		}
+	}
+
+	if(!cbFlower.checked()){
+		return;
+	}
+
+	for (let i = 0; i < (width*height)/3000; i++){
+		let flower = (random() < 0.5)? 255: 0;
+		let patchPosX = floor(random(width));
+		let patchPosY = floor(random(height));
+
+		for (let j = 0; j < 10; j++) {
+			let flowerPosX = patchPosX;
+			let flowerPosY = patchPosY;
+
+			for (let k = 0; k < 5; k++) {
+
+				flowerPosX = flowerPosX + floor(random(0,6)) - floor(random(0,6));
+				flowerPosY = flowerPosY + floor(random(0,6)) - floor(random(0,6));
+				if((flowerPosX >= 0 && flowerPosX < width) &&
+					(flowerPosY >= 0 && flowerPosY < height)){
+
+					let levelValue = sLevels[sFlower.value()].value();
+					if(noiseValues[flowerPosX + flowerPosY*width] == levelValue + 1){
+						stroke(255, flower, 0);
+						point(flowerPosX, flowerPosY);
+					}
+
+				}
+
+			}
+		}
+
 	}
 
 }
